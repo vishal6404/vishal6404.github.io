896aac91eb81ca69a5db07f2a09104408cf71cbe
Vishal <31859086+vishal6404@users.noreply.github.com>
2019-09-29 18:07:18 +0530

    Update a.js

--- nilu/a.js
+++ nilu/a.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Oct 7, 2019 00:00:00').getTime(),
+let countDown = new Date('Oct 6, 2019 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
