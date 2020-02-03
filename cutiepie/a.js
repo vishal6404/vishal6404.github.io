744142e499c31ae79ea057e73a1a68292c341b03
Vishal <31859086+vishal6404@users.noreply.github.com>
2020-02-04 02:35:09 +0530

    Shreya

--- cutiepie/a.js
+++ cutiepie/a.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('May 25, 2019 00:00:00').getTime(),
+let countDown = new Date('Aug 26, 2020 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
